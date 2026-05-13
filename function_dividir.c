60cc835c55a70331c6000275f25125407e29fc6a
valentinaandradep <valentinaandradedepedro1@gmail.com>
2026-05-13 19:27:13 -0300

    Fix formatting and indentation in dividir function

--- function_dividir.c
+++ function_dividir.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-int dividir(int numero1, int numero2) 
+int dividir(int numero1, int numero2)
 {
-    int dividendo, divisor;
-    int quociente, resto;
+    int dividendo, divisor;
+    int quociente, resto;
 
-    dividendo = numero1;
-    divisor = numero2;
-    quociente = 0;
-    resto = dividendo;
+    dividendo = numero1;
+    divisor = numero2;
+    quociente = 0;
+    resto = dividendo;
 
-    while (resto >= divisor) 
-    {
-        quociente = quociente + 1;
-        resto = resto - divisor;
-    }
+    while (resto >= divisor)
+    {
+     quociente = quociente + 1;
+     resto = resto - divisor;
+   }
 }
